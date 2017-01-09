50f09d22e239054079db3fc36056f7406ad5c2f9
Naveenreddivari <naveenreddyvari@gmail.com>
2017-01-09 19:21:59 +0530

    Session5-Assignment4
    Assignment4

---
+++ Assignment4.js
@@ -0,0 +1,7 @@
+var employee = [{ name: 'Naveen', age: 28, salary: 20000, address: { city: 'Bangalore',state: 'karnataka', pin: 560036 }},
+		{ name: 'manju', age: 24, salary: 30000, address: { city: 'hyderbad',state: 'andhrapradesh', pin: 560058 }},
+		{ name: 'Hari', age: 25, salary: 50000, address: { city: 'Bangalore',state: 'karnataka', pin: 560058 }},
+		{ name: 'hithu', age: 26, salary: 60000, address: { city: 'amaravathi',state: 'andhrapradesh', pin: 560000 }},
+		{ name: 'rama', age: 27, salary: 30000, address: { city: 'Bangalore',state: 'karnataka', pin: 560058 }}
+ ]
+console.log("The name is "+ employee[1].name +", age is "+employee[1].age +", salary "+employee[1].salary +", city is "+employee[1].address.city +", state is "+employee[1].address.state+", pincode is " + employee[1].address.pin);
